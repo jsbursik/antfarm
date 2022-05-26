071d51a4821f63f5376c7314aa8fdd64d806e5d1
Jordan Bursik <jsbursik@gmail.com>
2022-05-26 09:25:02 -0500

    Fixed express app routing

--- app.js
+++ app.js
@@ -5,13 +5,7 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + "/index.html");
 });
 
-app.get('/src/antfarm.js', (req, res) => {
-    res.sendFile(__dirname + "/src/antfarm.js");
-});
-
-app.get('/src/ant.js', (req, res) => {
-    res.sendFile(__dirname + "/src/ant.js");
-});
+app.use('/src', express.static('src'));
 
 app.listen(3000, () => {
     console.log('[Express] Listening on 3000');
